57cec119e891251fcacff9780d4a2a0c43f3d653
Dayron_Glez <dayronglez01@gmail.com>
2026-01-29 23:43:58 +0100

    feat: Add type for tas Priority and size

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,18 @@
+export type TaskPriority = "P0" | "P1" | "P2";
+export type TaskSize = "XS" | "S" | "M" | "L" | "XL";
+
 export type ColumnType = {
-    id: number | string;
-    title: string;
-}
+  id: number | string;
+  title: string;
+};
 
 export type Task = {
-    id: number | string;
-    columnId: number | string;
-    content: string;
-}
+  id: number | string;
+  columnId: number | string;
+  content: string;
+  priority: TaskPriority;
+  size: TaskSize;
+};
+
+export const TASK_PRIORITIES: TaskPriority[] = ["P0", "P1", "P2"];
+export const TASK_SIZES: TaskSize[] = ["XS", "S", "M", "L", "XL"];
