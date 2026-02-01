7e6b228cc9e37d18aa65c196debe1e9c17b90912
Dayron_Glez <dayronglez01@gmail.com>
2026-02-01 20:13:25 +0100

    feat: Add no results state for task filtering with appropriate messaging and image

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/sidebar";
 import { KanbanProvider, useKanban } from "@/context/KanbanContext";
 import noDataSvg from "@/assets/noData.svg";
+import notFindByFilter from "@/assets/notFindByFilter.svg";
 
 export const SearchContext = createContext<{
   searchValue: string;
@@ -18,8 +19,14 @@ export const SearchContext = createContext<{
 
 function MainContent() {
   const { state } = useSidebar();
-  const { scrollContainerRef, columns } = useKanban();
+  const { scrollContainerRef, columns, tasks } = useKanban();
   const [searchValue, setSearchValue] = useState<string>("");
+
+  const filteredTasks = tasks.filter((task) => {
+    const searchTerm = searchValue.trim().toLowerCase();
+    if (!task.content || !searchTerm) return true;
+    return task.content.toLowerCase().includes(searchTerm);
+  });
 
   return (
     <>
@@ -32,7 +39,23 @@ function MainContent() {
       >
         {columns && columns.length > 0 ? (
           <SearchContext.Provider value={{ searchValue, setSearchValue }}>
-            <Outlet />
+            {filteredTasks.length === 0 && searchValue.trim().length > 0 ? (
+              // Blank state cuando no hay coincidencias
+              <div className="flex flex-col items-center justify-center w-full h-full">
+                <img
+                  src={notFindByFilter}
+                  alt="Sin resultados"
+                  className="size-[500px]"
+                />
+                <p className="text-primary text-lg font-semibold">
+                  No hay tareas que coincidan con el filtro de búsqueda
+                  aplicado. Por favor, intente con otro término de búsqueda o
+                  elimine el filtro.
+                </p>
+              </div>
+            ) : (
+              <Outlet />
+            )}
           </SearchContext.Provider>
         ) : (
           <div className="flex flex-col items-center justify-center">
