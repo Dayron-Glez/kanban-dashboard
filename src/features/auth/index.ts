407b5c9f8b55413be4c839ffc4aebabfb6f8789a
Dayron_Glez <dayronglezdev@gmail.com>
2026-04-29 12:35:55 +0200

    feat(auth): exportar módulo auth desde index

---
+++ src/features/auth/index.ts
@@ -0,0 +1,4 @@
+export { AuthProvider, useAuth } from "./context/AuthContext"
+export { AuthGuard } from "./components/AuthGuard"
+export { LoginPage } from "./components/LoginPage"
+export { RegisterPage } from "./components/RegisterPage"
