78df09d30d408124cca8aa8060c57f4864de6cd9
Dayron_Glez <dayronglezdev@gmail.com>
2026-04-29 13:44:53 +0200

    refactor(layouts): envolver AppLayout con ProjectsProvider

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,20 +1,22 @@
 import { Outlet } from "react-router"
 import { Sidebar, SidebarInset, SidebarProvider } from "@/shared"
-import { ProjectSidebarContent } from "@/features/project"
+import { ProjectSidebarContent, ProjectsProvider } from "@/features/project"
 
 export default function AppLayout() {
   return (
-    <SidebarProvider defaultOpen={true} className="h-screen">
-      <Sidebar
-        collapsible="icon"
-        className="min-w-16 border-r-transparent shadow-md bg-background"
-      >
-        <ProjectSidebarContent />
-      </Sidebar>
+    <ProjectsProvider>
+      <SidebarProvider defaultOpen={true} className="h-screen">
+        <Sidebar
+          collapsible="icon"
+          className="min-w-16 border-r-transparent shadow-md bg-background"
+        >
+          <ProjectSidebarContent />
+        </Sidebar>
 
-      <SidebarInset className="h-screen flex flex-col overflow-hidden">
-        <Outlet />
-      </SidebarInset>
-    </SidebarProvider>
+        <SidebarInset className="h-screen flex flex-col overflow-hidden">
+          <Outlet />
+        </SidebarInset>
+      </SidebarProvider>
+    </ProjectsProvider>
   )
 }
